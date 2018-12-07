782387ba8f65a27c3e972ccf6f8fa50ae3e44838
Maxim Kugatov <maximkugatov@gmail.com>
2018-12-07 16:59:29 +0200

    add project

---
+++ src/main/java/com/kugmax/learn/digitgateway/digitgateway/DigitGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.kugmax.learn.digitgateway.digitgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitGatewayApplication.class, args);
+	}
+}
