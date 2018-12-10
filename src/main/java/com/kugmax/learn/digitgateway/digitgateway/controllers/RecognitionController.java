a2b760570caaa320e53557083afa03a44247700c
Maxim Kugatov <maximkugatov@gmail.com>
2018-12-10 18:07:09 +0200

    sotil

--- src/main/java/com/kugmax/learn/digitgateway/digitgateway/controllers/RecognitionController.java
+++ src/main/java/com/kugmax/learn/digitgateway/digitgateway/controllers/RecognitionController.java
@@ -1,15 +1,42 @@
 package com.kugmax.learn.digitgateway.digitgateway.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 
 @RestController
 public class RecognitionController {
 
-    @ResponseBody
-    @RequestMapping("/recognize")
-    public String recognize() {
-        return "recognized";
+//    @ResponseBody
+//    @RequestMapping("/recognize")
+//    public String recognize() {
+//        return "recognized";
+//    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(method = RequestMethod.POST, value = "/recognize")
+    public String recognize(HttpEntity<byte[]> requestEntity) throws Exception {
+//        process(requestEntity.getBody());
+
+//        BufferedImage bi = new BufferedImage();
+//        File outputfile = new File("saved.png");
+//        ImageIO.write(bi, "png", outputfile);
+
+        System.out.println("in recognize");
+
+        OutputStream out = new FileOutputStream("a.jpg");
+        out.write(requestEntity.getBody());
+        out.flush();
+        out.close();
+
+        return "OK";
+
     }
 }
