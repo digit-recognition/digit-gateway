6a1e7669ef5e01ba506933a775edc7364e8ce72c
Maxim Kugatov <maximkugatov@gmail.com>
2018-12-11 09:34:20 +0200

    it saves bytes as png

--- src/main/java/com/kugmax/learn/digitgateway/digitgateway/controllers/RecognitionController.java
+++ src/main/java/com/kugmax/learn/digitgateway/digitgateway/controllers/RecognitionController.java
@@ -1,42 +1,46 @@
 package com.kugmax.learn.digitgateway.digitgateway.controllers;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-
+import java.io.*;
 
 @RestController
 public class RecognitionController {
 
-//    @ResponseBody
-//    @RequestMapping("/recognize")
-//    public String recognize() {
-//        return "recognized";
-//    }
-
     @CrossOrigin(origins = "*")
     @RequestMapping(method = RequestMethod.POST, value = "/recognize")
     public String recognize(HttpEntity<byte[]> requestEntity) throws Exception {
-//        process(requestEntity.getBody());
-
-//        BufferedImage bi = new BufferedImage();
-//        File outputfile = new File("saved.png");
-//        ImageIO.write(bi, "png", outputfile);
 
         System.out.println("in recognize");
 
-        OutputStream out = new FileOutputStream("a.jpg");
-        out.write(requestEntity.getBody());
-        out.flush();
-        out.close();
+        InputStream in = null;
+        FileOutputStream fos = null;
+        try {
+            String imageString = IOUtils.toString(requestEntity.getBody(), "UTF-8");
+            System.out.println(imageString);
+
+
+            imageString = imageString.substring("data:image/png;base64,".length());
+            byte[] contentData = imageString.getBytes();
+            byte[] decodedData = Base64.decodeBase64(contentData);
+            String imgName = "a.png";
+            fos = new FileOutputStream(imgName);
+            fos.write(decodedData);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Failed";
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (fos != null) {
+                fos.close();
+            }
+        }
 
         return "OK";
-
     }
 }
