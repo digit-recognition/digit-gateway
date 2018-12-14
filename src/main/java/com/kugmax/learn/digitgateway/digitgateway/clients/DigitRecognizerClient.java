f4d2cdc8c95813ab3c072b25fe8808cbfaff5e21
Maxim Kugatov <maximkugatov@gmail.com>
2018-12-14 11:07:21 +0200

    modified:   .gitignore 	modified:   a.png 	modified:   pom.xml 	modified:   src/main/java/com/kugmax/learn/digitgateway/digitgateway/clients/DigitRecognizerClient.java 	modified:   src/main/java/com/kugmax/learn/digitgateway/digitgateway/controllers/RecognitionController.java 	modified:   src/main/proto/recognizer.proto

--- src/main/java/com/kugmax/learn/digitgateway/digitgateway/clients/DigitRecognizerClient.java
+++ src/main/java/com/kugmax/learn/digitgateway/digitgateway/clients/DigitRecognizerClient.java
@@ -1,4 +1,32 @@
 package com.kugmax.learn.digitgateway.digitgateway.clients;
 
+import com.kugmax.learn.digitgateway.digitgateway.services.DigitRecognitionRequest;
+import com.kugmax.learn.digitgateway.digitgateway.services.DigitRecognitionResponse;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import com.kugmax.learn.digitgateway.digitgateway.services.DigitRecognitionGrpc;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DigitRecognizerClient {
+
+    public int recognize(String filePath) {
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext(true)
+                .build();
+
+        DigitRecognitionGrpc.DigitRecognitionBlockingStub stub = DigitRecognitionGrpc.newBlockingStub(channel);
+
+
+        DigitRecognitionRequest request = DigitRecognitionRequest
+                .newBuilder()
+                .setValue(filePath)
+                .build();
+
+        DigitRecognitionResponse response = stub.recognize(request);
+
+        return response.getValue();
+    }
 }
--- src/main/java/com/kugmax/learn/digitgateway/digitgateway/controllers/RecognitionController.java
+++ src/main/java/com/kugmax/learn/digitgateway/digitgateway/controllers/RecognitionController.java
@@ -1,18 +1,22 @@
 package com.kugmax.learn.digitgateway.digitgateway.controllers;
 
+import com.kugmax.learn.digitgateway.digitgateway.clients.DigitRecognizerClient;
 import org.apache.commons.io.IOUtils;
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import java.io.*;
 
 @RestController
 public class RecognitionController {
+
+    @Autowired
+    private DigitRecognizerClient client;
 
     @CrossOrigin(origins = "*")
     @RequestMapping(method = RequestMethod.POST, value = "/recognize")
@@ -32,10 +36,12 @@ public class RecognitionController {
             fos.write(decodedData);
 
 
-            saveToMNIST(decodedData);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "Failed";
+            String path = saveToMNIST(decodedData);
+
+            int digit = client.recognize(path);
+            System.out.println(digit);
+
+            return String.valueOf(digit);
         } finally {
             if (in != null) {
                 in.close();
@@ -44,38 +50,20 @@ public class RecognitionController {
                 fos.close();
             }
         }
-
-        return "OK";
     }
 
-    private void saveToMNIST(byte[] decodedData) throws Exception {
+    private String saveToMNIST(byte[] decodedData) throws Exception {
         FileOutputStream fos = new FileOutputStream("gray.png");
         fos.write(decodedData);
         fos.close();
 
         BufferedImage image = ImageIO.read(new File("gray.png"));
 
-        int imgSize = 28;
-//
-//        for (int i = 0; i < imgSize; i++) {
-//            for (int j = 0; j < imgSize; j++) {
-//                int rgb = image.getRGB(i, j);
-//                int grayRGB = transformToGray(rgb);
-//                image.setRGB(i, j, grayRGB);
-//            }
-//        }
+        File toRecognize =  new File("gray2.png");
 
-//        ImageIO.write(image, "png", new File("gray2.png"));
-        ImageIO.write(RecognitionController.criaImagemCinza(image), "png", new File("gray2.png"));
-    }
+        ImageIO.write(RecognitionController.criaImagemCinza(image), "png", toRecognize);
 
-    private int transformToGray(int sourceRGB) {
-        int red   = (sourceRGB >> 16) & 0xFF;
-        int green = (sourceRGB >>  8) & 0xFF;
-        int blue  = (sourceRGB)       & 0xFF;
-        int gray  = (red * 3 + green * 6 + blue) / 10;
-
-        return gray;
+        return toRecognize.getAbsolutePath();
     }
 
     public static BufferedImage criaImagemCinza(BufferedImage imgJPEG) {
@@ -93,8 +81,6 @@ public class RecognitionController {
                 p[2] = (int) (0.11 * p[2]);
                 int y = p[0] + p[1] + p[2];
                 raster.setSample(w, h, 0, y);
-
-//                System.out.print(y + "|");
             }
         }
         return img;
